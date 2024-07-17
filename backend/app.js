fc192e468f11db33a583c3fe598d75efc792c55d
Angelestm <angelestm92@gmail.com>
2024-07-16 22:36:54 -0500

    Actualizando CORS

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,13 @@ const {errors} = require("celebrate");
 const cors = require('cors');
 const app = express();
 
-// inclúyelos antes de otras rutas
-app.use(cors());
-app.options('*', cors());
+// Configurar CORS con opciones
+const corsOptions = {
+  origin: '*', // Permitir cualquier origen
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permitir estos métodos
+  allowedHeaders: ['Content-Type', 'Authorization'] // Permitir estos encabezados
+};
+app.use(cors(corsOptions));
 
 // detecta el puerto 3000
 const {PORT = 3000} = process.env;
